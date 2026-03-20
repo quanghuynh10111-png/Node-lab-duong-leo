6c1c2829ae5bbe296619eda0d7e8c59bd5133892
Huỳnh Ngọc Dương <252091911+quanghuynh10111-png@users.noreply.github.com>
2026-03-20 16:10:25 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,3 +9,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Server chạy tại http://localhost:3000');
 });
+const userRoutes = require('./routes/user');
+app.use('/api/users', userRoutes);
