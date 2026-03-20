24a62bdebc66b4ccc119f850a17e4c94113df208
Huỳnh Ngọc Dương <252091911+quanghuynh10111-png@users.noreply.github.com>
2026-03-20 16:00:56 +0700

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Dương Leo!');
+});
+
+app.listen(3000, () => {
+  console.log('Server chạy tại http://localhost:3000');
+});
